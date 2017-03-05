8807303feb7acc5d727ae878f7ed701a6c233e91
Mick Dekkers <mickdekkersnl@gmail.com>
2017-03-05 01:03:37 +0100

    feat: normalize coordinates again

--- index.js
+++ index.js
@@ -8,6 +8,45 @@ const knex = require('knex')({
   useNullAsDefault: true
 })
 
+const compareCoord = (coord, point) => ({
+  min: R.min(point[coord]),
+  max: R.max(point[coord])
+})
+
+const getBounds = R.reduce((acc, point) => R.evolve({
+    x: compareCoord('x', point),
+    y: compareCoord('y', point),
+    z: compareCoord('z', point)
+  }, acc),
+  {
+    x: {
+      min: Number.POSITIVE_INFINITY,
+      max: Number.NEGATIVE_INFINITY
+    },
+    y: {
+      min: Number.POSITIVE_INFINITY,
+      max: Number.NEGATIVE_INFINITY
+    },
+    z: {
+      min: Number.POSITIVE_INFINITY,
+      max: Number.NEGATIVE_INFINITY
+    }
+  })
+
+const normalizeCoord = (coord, bounds) => (value) => ((value - bounds[coord].min) / bounds[coord].range)
+
+const normalizePoints = (bounds, points) => {
+  bounds.x.range = bounds.x.max - bounds.x.min
+  bounds.y.range = bounds.y.max - bounds.y.min
+  bounds.z.range = bounds.z.max - bounds.z.min
+
+  return R.map(R.evolve({
+    x: normalizeCoord('x', bounds),
+    y: normalizeCoord('y', bounds),
+    z: normalizeCoord('z', bounds)
+  }), points)
+}
+
 const getRegions = () => knex('mapRegions')
   .select(['regionID', 'regionName'])
   .reduce((acc, region) => {
@@ -34,6 +73,7 @@ const getSolarSystems = (regions) => knex('mapSolarSystems')
   ])
   // Skip wormhole systems
   .where('solarSystemID', '<', 31000000)
+  // Grab the info we want
   .map((solarSystem) => {
     return {
       name: solarSystem.solarSystemName,
@@ -45,6 +85,8 @@ const getSolarSystems = (regions) => knex('mapSolarSystems')
       z: solarSystem.z
     }
   })
+  // Normalize coordinates
+  .then(R.converge(normalizePoints, [getBounds, R.identity]))
 
 const generateJson = async (outputPath, indentation = 2) => {
   const regions = await getRegions()
